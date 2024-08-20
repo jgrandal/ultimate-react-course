b9043375f8f72b26dee58e772cd96fa21b20cf0c
Jorge Granda <jorge.granda@wiqu.io>
2024-08-20 23:59:25 +0200

    section 12 - effects and code without comments

--- 23-exercises/07-usepopcorn/src/App.js
+++ 23-exercises/07-usepopcorn/src/App.js
@@ -53,22 +53,14 @@ const average = (arr) =>
 
 const APIKEY = 'b442b664';
 
-// Task 1: Syncrhonising the query (piece of state) with movies (piece of state).
-// Task 2: Allow users to select a movie from the list so they can see some details about the movie.
-// Task 3: Load Movie Details based on the selected ID of a Movie.
-// Task 4: Adding a Watched Movie.
-// Task 5: Changing the page title with an Effect
-// Task 6: Cleaning Up the Title
-// Task 7: Cleaning Up Data Fetching
-// Task 8: Listening to a Keypress
 export default function App() {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
-  // This piece of state contains an array of WatchedMovie objects with the data to be rendered in the WatchedMovieList
+
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
-  // Create a new piece of state to store which movie ID have been selected
+
   const [selectedId, setSelectedId] = useState(null);
 
   function handleQuery(newQuery) {
@@ -76,9 +68,6 @@ export default function App() {
   }
 
   function handleSelectMovie(id) {
-    // setSelectedId(id);
-    // If user make double click over the movie then the movie details dissapear to show the statistics
-    // For this, if the selecteId is equal to the id then return null to dissapear the movie details else return the id to show the movie details
     setSelectedId((selectedId) => (selectedId === id ? null : id));
   }
 
@@ -94,20 +83,14 @@ export default function App() {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
 
-  // As soon as a new request is fired off then the previous one will stop.
-  // Abort Controller.
-  // Each that a new keystroke is added then the component is re-rendered, so the cleanup function is called and
-  // abort the previous request (request cancel). However, JavaScript sees this as an error so JS will throw an error
-  // that will be catched and render.
   useEffect(
     function () {
-      // Step 1: Create an abort controller.
       const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true);
-          setError(''); // Always before start fetching for data, reset the error state.
-          // Step 2: Connect the AbortController with the fetch function by passing a second argument which is an object with the signal property.
+          setError('');
+
           const response = await fetch(
             `http://www.omdbapi.com/?apikey=${APIKEY}&s=${query}`,
             { signal: controller.signal }
@@ -124,22 +107,17 @@ export default function App() {
           }
 
           setMovies(data.Search);
-          // Step 5: Set the error to empty after the movies have been set
           setError('');
         } catch (err) {
-          console.error(err.message);
-          // Step 4: Fix the Abort Error by abort requests
           if (err.name !== 'AbortError') {
+            console.log(err.message);
             setError(err.message);
           }
         } finally {
           setIsLoading(false);
-          //  //
         }
       }
 
-      // Fix the problem that when there isn't a search query (it is empty) then no error should be showed.
-      // When the query is empty (initial render or not input user), it actually don't even go to search anything.
       if (query.length < 3) {
         setMovies([]);
         setError('');
@@ -148,12 +126,10 @@ export default function App() {
 
       fetchMovies();
 
-      // Step 3: Define the cleanup function
       return function () {
         controller.abort();
       };
     },
-    // query should be added to the dependency array in order to be track any change on the piece of state and execute the effect.
     [query]
   );
 
@@ -286,13 +262,11 @@ function Movie({ movie, onSelectMovie }) {
   );
 }
 
-// Task 2: Create new component that will be displayed and render movie details when there is a selectedId (a movie is selected)
 function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [movieDetails, setMovieDetails] = useState({});
   const [isLoadingMovieDetails, setIsLoadingMovieDetails] = useState(false);
   const [userRating, setUserRating] = useState(0);
 
-  // Destructuring the MovieDetails to normalize variable names and use them in the JSX.
   const {
     Title: title,
     Year: year,
@@ -306,11 +280,8 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     Genre: genre,
   } = movieDetails;
 
-  // Derived State to calculate if a movie was watched.
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
-  // watched.filter((movie) => movie.imdbID === selectedId).length === 1;
 
-  // Derived State to get the user rating of a watched movie.
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId
   )?.userRating;
@@ -329,8 +300,6 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onCloseMovie();
   }
 
-  // Task 3: Whenever the movie details component is going to mount, fetch the movie details corresponding to the selected ID prop.
-  //         Loading the currently selected movie each time that the component is mounted
   useEffect(
     function () {
       async function getMovieDetails() {
@@ -347,34 +316,17 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     [selectedId]
   );
 
-  // Always use different effects for different things because each effect has only one purpose and it does one thing.
   useEffect(
     function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
-      // A cleanup function is needed whenever the side effect that was introduced in the effect
-      // keeps happening after the component has already been unmounted.
-      // return () => (document.title = 'usePopcorn');
       return function () {
         document.title = 'usePopcorn';
-        // Why this return the name of the movie if the clean up function runs after
-        // the component has already been unmounted (dissapear from the component tree including states, etc.)?.
-        // How will the function "remember" the value of the title variable?
-        // This is caused for a closure. A closure means that a function will always remember all the variables
-        // that were present at the time and the place data function was created
-        // In the case of the cleanup function, it was created by the time the effect first was created. And so, by
-        // that time the title was actually defined with a value. -> The cleanup function close over the title variable
-        // so it will be remembered in the future even after the component has already unmounted.
         console.log(`Clean up effect for movie ${title}`);
       };
     },
     [title]
   );
-
-  // As soon as the user press the ESC key then the MovieDetails component instance should be unmounted.
-  // The way to react to a keypress event in an entire app is by attaching an event listener to the entire document.
-  // Since this is a side effect because it is directly interacting with the DOM then the useEffect hook should be used.
-  // Step 1: Create a new effect using the useEffect hook in the App component
   useEffect(
     function () {
       function callback(e) {
@@ -383,22 +335,8 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
           console.log('Closing...');
         }
       }
-      // By doing some dome manipulations you are steping out of the React appplication
-      // That's the reason of the useEffect hook should be used.
-      // Step 2: Because the event listener should listen just when the MovieDetails component is mounted then move
-      //         to that componet. By this, the event listener just is attached to the document when the MovieDetails
-      //         component is actually mounted and present in the component tree.
       document.addEventListener('keydown', callback);
-
-      // In this effect the cleanup functions prevents that:
-      // - Each time the MovieDetails component is mounted then a new event listener is attached to the document.
-      // - Therefore, without the cleanup function to remove them, perfomance issues could appear by accumulate
-      //   all the attached event listeners to the document.
-      // As soon as the MovieDetails component unmounts, the event listener will then be removed from the document.
-      // With this, the event listener just be executed once.
       return function () {
-        // The callback function passed to remove the event listener should be the same that was passed to the
-        // add event listener.
         document.removeEventListener('keydown', callback);
       };
     },
