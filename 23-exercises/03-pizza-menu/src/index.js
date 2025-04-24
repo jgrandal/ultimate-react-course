937404d9b5461dba5925f7a088e2398a4f4527db
jgrandal <jorge.granda.leon@gmail.com>
2025-04-24 18:52:57 +0200

    Add pizza-menu exercise

--- 23-exercises/03-pizza-menu/src/index.js
+++ 23-exercises/03-pizza-menu/src/index.js
@@ -1,17 +1,137 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const pizzaData = [
+  {
+    name: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
+    price: 6,
+    photoName: 'pizzas/focaccia.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Margherita',
+    ingredients: 'Tomato and mozarella',
+    price: 10,
+    photoName: 'pizzas/margherita.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Spinaci',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    price: 12,
+    photoName: 'pizzas/spinaci.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Funghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    price: 12,
+    photoName: 'pizzas/funghi.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Salamino',
+    ingredients: 'Tomato, mozarella, and pepperoni',
+    price: 15,
+    photoName: 'pizzas/salamino.jpg',
+    soldOut: true,
+  },
+  {
+    name: 'Pizza Prosciutto',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    price: 18,
+    photoName: 'pizzas/prosciutto.jpg',
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  // const style = {
+  //   color: 'red',
+  //   textAlign: 'center',
+  //   fontSize: '2rem',
+  //   textTransform: 'uppercase',
+  //   letterSpacing: '0.1em',
+  //   margin: '1rem 0',
+  // };
+
+  const style = {};
+
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+      <p>We make the best pizza in town</p>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      {pizzaData.map((pizza) => (
+        <Pizza key={pizza.name} pizzaObj={pizza} />
+      ))}
+    </main>
+  );
+}
+
+function Pizza({ pizzaObj }) {
+  return (
+    <div className="pizza">
+      <img src={pizzaObj.photoName} alt={`Pizza ${pizzaObj.name}`} />
+      <div>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}</span>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) {
+  //   console.log('We are open!');
+  // } else {
+  //   console.log('We are closed!');
+  // }
+
+  // console.log(hour);
+  // const open = hour >= 8 && hour <= 22;
+  // console.log(open);
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()} .We're currently open.
+      {/* <p>All rights reserved</p> */}
+    </footer>
+  );
+}
+
+// React 18+ way to render the app
+// Create a root element and render the App component
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
