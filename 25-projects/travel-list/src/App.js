be5ee8dc289f19755f3dc44ec8446a688a1ddcc9
jgrandal <jorge.granda.leon@gmail.com>
2025-04-26 08:49:41 +0200

    feat(travel-list): implement initial app structure and static components

--- 25-projects/travel-list/src/App.js
+++ 25-projects/travel-list/src/App.js
@@ -1,5 +1,62 @@
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: true },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
+
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip? 😍</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li className="item">
+      <input type="checkbox" value={item.packed} />
+      <span className={item.packed ? 'packed' : ''}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
 
 export default App;
