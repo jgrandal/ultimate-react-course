aeb65bcd84c5ff56273cbf41a16e9d50975e0726
jgrandal <jorge.granda.leon@gmail.com>
2025-04-26 14:00:47 +0200

    feat(form): implement form with controlled elements (quantity and description inputs)

--- 25-projects/travel-list/src/App.js
+++ 25-projects/travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: true },
   { id: 2, description: 'Socks', quantity: 12, packed: true },
@@ -20,10 +22,54 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(`quantity: ${quantity} description: ${description}`);
+
+    if (!description) return;
+
+    const newItem = {
+      id: crypto.randomUUID(),
+      description,
+      quantity,
+      packed: false,
+    };
+    console.log(newItem);
+    // Reset the form
+    setDescription('');
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 😍</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => (
+          <option key={i + 1} value={i + 1}>
+            {i + 1}
+          </option>
+        ))}
+
+        {/* {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))} */}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
